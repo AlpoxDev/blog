0e59a728fe1aad71062e6d28726a19dd5a42a3a8
alpoxdev <alpoxdev@gmail.com>
2021-04-19 03:50:38 +0900

    add: PostDetail done

--- client/src/components/organism/Post/index.ts
+++ client/src/components/organism/Post/index.ts
@@ -2,6 +2,7 @@ export * from './PostList';
 export * from './PostItem';
 export * from './PostThumbnail';
 export * from './PostContent';
+export * from './PostDisqus';
 
 export * from './SkeletonList';
 export * from './SkeletonItem';
--- client/src/containers/post/detail.tsx
+++ client/src/containers/post/detail.tsx
@@ -9,7 +9,9 @@ import { observer } from 'mobx-react-lite';
 // components
 import { Text, Content } from 'components/atom';
 import { spacing } from 'common';
-import { PostContent } from 'components/organism';
+import { PostContent, PostDisqus } from 'components/organism';
+
+const isDEV = process.env.NODE_ENV === 'development';
 
 const Container = (): React.ReactElement => {
   const router = useRouter();
@@ -33,6 +35,7 @@ const Container = (): React.ReactElement => {
       <Text.H3 location={{ top: spacing(8) }}>{post.data?.subtitle}</Text.H3>
 
       <PostContent content={post.data?.content} />
+      {post.isReady && <PostDisqus url={'https://alpox.dev'} postId={post.data?.id} postTitle={post.data?.title} />}
     </PostDetailWrapper>
   );
 };
