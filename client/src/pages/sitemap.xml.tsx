be6a4c5c7dddc161502634f1773920638d03e8ab
alpoxdev <alpoxdev@gmail.com>
2021-04-27 04:01:19 +0900

    fix: sitemap

--- client/src/pages/sitemap.xml.tsx
+++ client/src/pages/sitemap.xml.tsx
@@ -3,18 +3,22 @@ import React from 'react';
 
 const Sitemap: React.FC = () => null;
 
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  if (res) {
-    res.setHeader('Content-Type', 'text/xml');
-    res.write(`<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+const defaultSitemap = `
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+ xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+</urlset>
+`;
 
-    </urlset>`);
-    res.end();
-  }
-  return {
-    props: {},
-  };
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  if (!res) return { props: {} };
+
+  const sitemap = defaultSitemap;
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
 };
 
 export default Sitemap;
